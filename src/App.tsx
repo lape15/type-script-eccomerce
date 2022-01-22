0d454bf8c3294a3be49ab47636718ad69b0541f6
Lape15 <larape1993@gmail.com>
2022-01-22 12:09:55 +0100

    Updating layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 import color from './assets/color';
 import { increment } from './slices/test';
 import { RootState } from './store';
+import LayOut from './layout';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -12,30 +13,7 @@ function App() {
   const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!.</p>
-        <p>
-          <button type="button" onClick={() => dispatch(increment())}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code style={{ color: color.orange }}> App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer">
-            Vite Docs
-          </a>
-        </p>
-      </header>
+     <LayOut title="hello" />
     </div>
   );
 }
---
+++ src/layout/index.tsx
@@ -0,0 +1,10 @@
+
+type Props = {
+    title: string
+}
+
+const LayOut = (props:Props) => {
+ return <div>{props.title}Here is where Layout for that bloody shoeee might beeee</div>;
+}
+
+export default LayOut
