2126586d5f5f0709e3bddc0d7dc3be6d2d2144b6
Lape15 <larape1993@gmail.com>
2022-01-23 21:36:31 +0100

    Hompage view added

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,6 @@
 import './card.scss'
+import cart from '../../assets/images/icon-cart-white.svg'
+
 const Card = () => {
     return <div className="block  flex ">
         <h6 className="title">SNEAKER COMPANY</h6>
@@ -13,12 +15,16 @@ const Card = () => {
                 </h4>
                 <div className='flex-item items-center m-1'>50%</div>
             </div>
-            <div className='flex-item'>
-            <span>$250.00</span>
-            </div>
-            <div className='flex-item'>
-                <button>0</button>
-                <button> Add to cart</button>
+            <div className='flex-item mt-1 btns'>
+                <div className='flex-item'>
+                <button className='btn'>&#8722;</button>
+                <button className='btn zero'>0</button>
+                <button className='btn'>&#43;</button>
+                </div>
+                <div className='flex-item add_cart'>
+                <button className='cart_btn'><img src={cart} alt='cart'/> Add to cart</button>
+                </div>
+              
             </div>
         </section>
     </div>
